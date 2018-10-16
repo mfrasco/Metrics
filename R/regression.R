9745c877465610444596f0113ec0e77f860db74d
AdvikS <AdvikS@users.noreply.github.com>
2018-10-16 11:20:51 -0500

    explained_variation docs typo

--- R/regression.R
+++ R/regression.R
@@ -350,7 +350,7 @@ rae <- function(actual, predicted) {
 #'
 #' @inheritParams params_regression
 #' @export
-#' @seealso \cord{\link{rse}}
+#' @seealso \code{\link{rse}}
 #' @examples
 #' actual <- c(1.1, 1.9, 3.0, 4.4, 5.0, 5.6)
 #' predicted <- c(0.9, 1.8, 2.5, 4.5, 5.0, 6.2)
